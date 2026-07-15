c855313e499dfdfee0ccb5d77fe76b66e825c668
Adam Lesniak <hello@alesniak.com>
2026-07-15 12:18:46 +0200

    feat: Story 4 - remove summary page

--- frontend/libs/input-lib/src/lib/summary/summary.component.ts
+++ frontend/libs/input-lib/src/lib/summary/summary.component.ts
@@ -1,5 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, inject } from '@angular/core';
 import { NxColComponent, NxLayoutComponent, NxRowComponent } from '@aposin/ng-aquila/grid';
 
 import { InputStore } from '../input-lib/store/input.store';
@@ -10,13 +9,6 @@ import { InputStore } from '../input-lib/store/input.store';
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.css',
 })
-export class SummaryComponent implements OnInit {
+export class SummaryComponent {
   protected readonly inputStore = inject(InputStore);
-  private readonly router = inject(Router);
-
-  ngOnInit(): void {
-    if (this.inputStore.uiState().quote.basisdaten.alterBeiRentenbeginn === 0) {
-      this.router.navigate(['/']);
-    }
-  }
 }
